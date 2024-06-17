b504e659e2f34720f4d67c3e5f6319bb5da29ad3
toms74209200 <motomotomato@gmail.com>
2024-06-17 23:01:35 +0900

    :sparkles: Update firmware

--- firmware/order66/keymaps/default/keymap.c
+++ firmware/order66/keymaps/default/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_UP,   KC_GRV, \
-        MO(_MAC_CTRL), KC_LALT, KC_LGUI, KC_LANG2,KC_SPC,  KC_SPC,  KC_SPC,  KC_LANG1,KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT ),
+        MO(_MAC_CTRL), KC_LALT, KC_LGUI, KC_LNG2,KC_SPC,  KC_SPC,  KC_SPC,  KC_LNG1,KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT ),
 
     [1] = LAYOUT(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, \
