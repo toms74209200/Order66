7f26da091c8740a67b7c6b803792c0781d0aeb79
toms74209200 <motomotomato@gmail.com>
2024-08-26 20:12:29 +0900

    :bug: Fix resolve resources

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [mdx()],
+  base: "/Order66",
+  site: "https://toms74209200.github.io/Order66/"
+});
